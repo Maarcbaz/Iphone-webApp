fe27652061fd05c25caec9dea5994fff7452a987
Maarcbaz <138406453+Maarcbaz@users.noreply.github.com>
2023-12-27 23:31:08 +0100

    overlay nav

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-let search__field, search;
+let search__field, search, menu, overlay;
 search__field = document.querySelector('#search')
 search = document.querySelector('#search__icon')
+
+menu = document.querySelector('.menu__icon')
+overlay = document.querySelector('.overlay')
 
 search.addEventListener('click', () => {
     if(search__field.style.display == 'block') {
@@ -8,5 +11,14 @@ search.addEventListener('click', () => {
     }
     else {
         search__field.style.display = 'block'
+    }
+})
+
+menu.addEventListener('click', () => {
+    if(overlay.style.width == '250px') {
+        overlay.style.width = '0'
+    }
+    else {
+        overlay.style.width = '250px'
     }
 })
